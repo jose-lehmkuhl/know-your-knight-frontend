78ec916d30618e9bdfdea08c53c82ad201d89036
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-03-28 14:42:16 -0300

    Header styling done

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Grid,Typography } from '@material-ui/core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChessKnight } from '@fortawesome/free-solid-svg-icons'
 
 import { HeaderLink } from './styles'
 
 const Header = () => (
     <Grid container>
         <HeaderLink to="/">
-            <Grid container item xs={12} md={6} justify="flex-start">
-                <Typography variant="h5">
+            <Grid container item xs={12} justify="center" alignItems="center">
+            <FontAwesomeIcon icon={faChessKnight} size="2x"/>
+                <Typography variant="h4">
                     Knight Moves
                 </Typography>
+            <FontAwesomeIcon icon={faChessKnight} size="2x" flip="horizontal"/>
             </Grid>
         </HeaderLink>
     </Grid>
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 
 export const HeaderLink = styled(Link)`
     width: 100%;
-    background: blue;
     padding: 10px;
     text-decoration: none;
     border-radius: 8px 8px 0 0;
-    box-shadow: 0px 1.5px 3px 0.5px rgba(0,0,0,0.75);
+    color: #3f51b5;
+
+    &:hover {
+        color: #303f9f;
+    }
 `
 
