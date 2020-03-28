30a53d0ebe1e9843dd415604f2554c0b4e0f75eb
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-03-28 15:53:22 -0300

    Home page styled

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Grid, Button } from '@material-ui/core'
+import { Grid, Button, Typography } from '@material-ui/core'
 import { ButtonWrapper } from './styles'
+
+import chessboard from '../../assets/images/chessboard.png';
+
 
 const Home = () => (
     <Grid container>
-        <Grid container item xs={12} md={6} justify="center">
-            <ButtonWrapper to="/help">
-                <Button variant="contained"  fullWidth>
-                    help
+        <Grid container item xs={12} justify="center">
+            <ButtonWrapper to="/board">
+                <Button variant="contained" color="primary" fullWidth>
+                    <Grid container item direction='column' alignItems="center" justify="center">
+                        <img src={chessboard}/>
+                        <Typography>go to board</Typography>
+                    </Grid>
                 </Button>
             </ButtonWrapper>
         </Grid>
-        <Grid container item xs={12} md={6} justify="center">
-            <ButtonWrapper to="/board">
-                <Button variant="contained" fullWidth>
-                    board
+        <Grid container item xs={12} justify="center">
+
+            <ButtonWrapper to="/help">
+                <Button variant="contained"  fullWidth>
+                    help
                 </Button>
             </ButtonWrapper>
         </Grid>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 export const ButtonWrapper = styled(Link)`
     margin: 10px;
     width: 100%;
+    max-width: 500px;
     text-decoration: none;
+
+
+    img {
+        width: 80%;
+        height: auto;
+        margin: 1rem 0;
+    }
 `
 
